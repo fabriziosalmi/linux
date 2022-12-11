97b2d8ebd97fe815a030cead4e9fd81da277d544
fab <fabrizio.salmi@gmail.com>
2022-12-11 11:01:02 +0100

    Create cloudflared-ubuntu22.sh

---
+++ cloudflared-ubuntu22.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# curl -s http://server/path/script.sh | bash -s
+
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+sudo apt-get update && sudo apt-get install -y cloudflared
