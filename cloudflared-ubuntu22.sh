37e0738393f72f0ec1d4d1ef16fbe5e24cf6f778
fab <fabrizio.salmi@gmail.com>
2023-03-02 13:57:04 +0100

    Update cloudflared-ubuntu22.sh

--- cloudflared-ubuntu22.sh
+++ cloudflared-ubuntu22.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# curl -s https://raw.githubusercontent.com/fabriziosalmi/linux/master/cloudflared-ubuntu22.sh | bash -s
+# apk add bash curl grep && curl -s https://raw.githubusercontent.com/fabriziosalmi/linux/master/cloudflared-ubuntu22.sh | bash -s
 
 sudo mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
